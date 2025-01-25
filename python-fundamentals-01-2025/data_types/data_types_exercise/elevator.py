63347b97db60490838c5a28a87711645bb53275f
tedi-tech-enthusiast <t.koparanova@abv.bg>
2025-01-25 12:28:05 +0200

    Delete python-fundamentals-01-2025/data_types/data_types_exercise directory

--- python-fundamentals-01-2025/data_types/data_types_exercise/elevator.py
+++
@@ -1,9 +0,0 @@
-from math import ceil
-
-number_of_people = int(input())
-elevator_capacity = int(input())
-
-number_of_lifts = ceil(number_of_people / elevator_capacity)
-
-print(number_of_lifts)
-
--- python-fundamentals-01-2025/data_types/data_types_exercise/print_part_of_the_ASCII_table.py
+++
@@ -1,9 +0,0 @@
-first_index = int(input())
-second_index = int(input())
-
-for index in range(first_index, second_index + 1):
-
-    if index == second_index:
-        print(chr(index))
-    else:
-        print(chr(index), end=' ')
